acda430837998b5df4c0c7f1ef8b563cace6442f
lecht <lecht42@gmail.com>
2022-06-03 19:20:19 +0300

    App bar is deleted and some refactoring

--- lib/homepage/my_home_page.dart
+++ lib/homepage/my_home_page.dart
@@ -3,9 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -16,23 +14,24 @@ class _MyHomePageState extends State<MyHomePage> {
   var _textColor = Colors.black;
 
   void _changeColor() {
-    List<int> getRandomColor() {
+    const byteMaxValue = 255;
+    final rgb = () {
       final rnd = Random(DateTime.now().microsecond);
       const maxColor = 256;
+
       return [
         rnd.nextInt(maxColor),
         rnd.nextInt(maxColor),
         rnd.nextInt(maxColor)
       ];
-    }
-
-    const byteMaxValue = 255;
-    final rgb = getRandomColor();
+    }();
 
     setState(() {
       _color = Color.fromARGB(byteMaxValue, rgb[0], rgb[1], rgb[2]);
       _textColor = Color.fromARGB(byteMaxValue,
-          byteMaxValue - rgb[0], byteMaxValue - rgb[1], byteMaxValue - rgb[2]);
+                                  byteMaxValue - rgb[0],
+                                  byteMaxValue - rgb[1],
+                                  byteMaxValue - rgb[2]);
     });
   }
 
@@ -40,11 +39,10 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     const duration = Duration(seconds: 1);
     const fontSize = 42.0;
+    const shadow = [Shadow(offset: Offset(1.0, 1.0), color: Colors.black)];
+    const text = 'Hey there';
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
       body: GestureDetector(
         onTap: () => _changeColor(),
         child: AnimatedContainer(
@@ -56,13 +54,11 @@ class _MyHomePageState extends State<MyHomePage> {
             child: AnimatedDefaultTextStyle(
               style: TextStyle(
                 color: _textColor,
-                shadows: const [
-                  Shadow(offset: Offset(1.0, 1.0), color: Colors.black),
-                ],
+                shadows: shadow,
                 fontSize: fontSize,
               ),
               duration: duration,
-              child: const Text('Hey there'),
+              child: const Text(text),
             ),
           ),
         ),
