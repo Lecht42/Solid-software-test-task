e481a9faeb85cb1b08416bcb68aba9ba4865fc65
lecht <lecht42@gmail.com>
2022-06-05 12:22:12 +0300

    Merge remote-tracking branch 'origin/main'

--- lib/homepage/my_app.dart
+++ lib/homepage/my_app.dart
@@ -7,12 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Test task',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
